32a7d8bb0f221e37279abba18765e46f2770d62c
khaledosama007 <khaledosama500@gmail.com>
2016-04-19 18:53:55 +0200

    Messages Interface

--- app/src/main/java/team/fcisquare/MessagesFragment.java
+++ app/src/main/java/team/fcisquare/MessagesFragment.java
@@ -5,12 +5,18 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 /**
  * Created by Andrew on 4/17/2016.
  */
 public class MessagesFragment extends Fragment {
     private View view;
+    //Message String  , Sender Name , Read Or not ,to be extracted from objects retrieved
+    //from DB
+    String[] Messages = {"Message Test"}; // To be replaced by the list of messages
+    String[] Senders = {"Sender Name"};
+    boolean [] readness = new boolean[1]; // If the message is read -> 1 , else -> 0
 
     public MessagesFragment() {
         super();
@@ -18,13 +24,17 @@ public class MessagesFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        readness[0] = true;
         view = inflater.inflate(R.layout.message_fragment, container, false);
+        ListView li = (ListView) view.findViewById(R.id.mlist);
+        li.setAdapter(new MessageList(getActivity() , Messages , readness , Senders));
         return view;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+       // MessageList adapter = new MessageList(MessagesFragment. , test);
 
     }
 }
